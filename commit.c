9221e0d79bdc99192694f579466430d0fdcfaa39
Manasa <manasakyadav28@gmail.com>
2026-04-20 18:34:58 +0000

    Phase 4: Add head_update to atomically swing branch pointer

--- commit.c
+++ commit.c
@@ -225,3 +225,4 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     
     return head_update(commit_id_out);
 }// Phase 4: head_read - resolves HEAD through symbolic refs
+// Phase 4: head_update - atomically swings branch pointer
